54cc3277d2711b5d734f3f6fa12d621a39b259ea
Ayoub Boukentar <aboukent@c4r3p2.1337.ma>
2025-04-08 06:11:27 +0100

    basic full code still need debugging

--- main.c
+++ main.c
@@ -144,15 +144,44 @@ int	best_position(t_stack *a, int nb)
 	}
 	return (i);
 }
+int	find_max(t_stack *b)
+{
+	int		i;
+	int		j;
+	int		nb;
+	t_stack	*tmp;
+
+	i = 0;
+	j = 0;
+	tmp = b;
+	nb = b->data;
+	while (tmp)
+	{
+		if (nb < tmp->data)
+		{
+			nb = tmp->data;
+			i = j;
+		}
+		j++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
+
 void	indexing(t_stack **a, t_stack **b)
 {
 	int i;
 	int min;
 	int	max;
 	int	sorted[count(*a) + 1];
+	int	chunk;
 	t_stack	*tmp;
 
 	tmp = *a;
+	if (count(*a) <= 100)
+		chunk = 6;
+	else
+		chunk = 12;
 	while (tmp)
 	{
 		sorted[best_position(*a, tmp->data)] = tmp->data;
@@ -160,14 +189,14 @@ void	indexing(t_stack **a, t_stack **b)
 	}
 	i = 0;
 	min = 0;
-	max = count(*a) / 6;
+	max = count(*a) / chunk;
 	// printf("%d\n", a->data);
 	// ra(&a);
 	// pb(a, b);
 	// pb(a, b);
 	// rb(&b);
 	// printf("%d\n", a->data);
-	if ((*a)->next)
+	while ((*a)->next)
 	{
 		if ((*a)->data <= sorted[min])
 		{
@@ -181,12 +210,29 @@ void	indexing(t_stack **a, t_stack **b)
 			pb(a, b);
 			if ((*b)->data < (*b)->next->data)
 				sb(b);
-			min++;
+			min++;  
 			max++;
 		}
 		else
 			ra(a);
 	}
+	int	mid = count(*b) / 2;
+	int	pos;
+	while (*b)
+	{
+		pos = find_max(*b);
+		if (pos <= mid)
+		{
+			while(pos-- > 0)
+				ra(b);
+		}
+		else
+		{
+			while(pos-- > 0)
+				ra(b);
+		}
+		pa(a, b);
+	}			
 }
 int main(int ac, char **av)
 {
@@ -205,7 +251,7 @@ int main(int ac, char **av)
 	// rra(&a);rrb(&b);rrr(&a, &b);
 	// printf("----\n");
 	// i = 0;
-	t_stack *tmp = a;
+	// t_stack *tmp = a;
 	// while(tmp)
 	// {
 	// 	printf("%d\n", tmp->data);
@@ -213,22 +259,22 @@ int main(int ac, char **av)
 	// }
 	indexing(&a, &b);
 	// sorting(a, b);
-	printf("----\n");
-	i = 0;
-	tmp = a;
-	while(tmp)
-	{
-		printf("%d\n", tmp->data);
-		tmp = tmp->next;
-	}
-	printf("----\n");
-	i = 0;
-	tmp = b;
-	while(tmp)
-	{
-		printf("%d\n", tmp->data);
-		tmp = tmp->next;
-	}
+	// printf("----\n");
+	// i = 0;
+	// tmp = a;
+	// while(tmp)
+	// {
+	// 	printf("%d\n", tmp->data);
+	// 	tmp = tmp->next;
+	// }
+	// printf("----\n");
+	// i = 0;
+	// tmp = b;
+	// while(tmp)
+	// {
+	// 	printf("%d\n", tmp->data);
+	// 	tmp = tmp->next;
+	// }
 	free_stack(&a);
 	free_stack(&b);
 }
